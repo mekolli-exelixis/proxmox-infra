548b05af297c4dc3a544d651c928740637c62c59
Meher Kolli <mekolli@exelixis.com>
2026-02-23 15:26:54 -0800

    Update uservm.tf

--- modules/templates/uservm.tf
+++ modules/templates/uservm.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "3.0.2-rc07"
+    }
+  }
+}
+
 module "uservms" {
   source = "../vm"
   vm_count = 8
