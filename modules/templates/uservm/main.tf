a69ed0cf3e9d2cb861cdd9d0e4de78fc40af3cbc
Meher Kolli <mekolli@exelixis.com>
2026-02-23 15:53:55 -0800

    Create main.tf

---
+++ modules/templates/uservm/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "3.0.2-rc07"
+    }
+  }
+}
+
+variable "template_type" {
+  type = string
+}
+
+module "uservm" {
+  source      = "../../vm"
+  vm_count    = 4
+  base_name   = "user-vm-a"
+  start_vmid  = 500
+  cpu         = 1
+  memory      = 1024
+  template    = var.template_type
+}
