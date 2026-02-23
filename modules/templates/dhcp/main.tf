d32b23de072fdf9d6e4c5e5152fd12fc968e1bb0
Meher Kolli <mekolli@exelixis.com>
2026-02-23 15:51:41 -0800

    Create main.tf

---
+++ modules/templates/dhcp/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "3.0.2-rc07"
+    }
+  }
+}
+
+variable "template_type" {
+  type = string
+}
+
+module "dhcp" {
+  source      = "../../vm"
+  vm_count    = 2
+  base_name   = "dhcp-vm-a"
+  start_vmid  = 200
+  cpu         = 4
+  memory      = 8096
+  template    = var.template_type
+}
