0885e8d8b7a9c1b1aa0a995935f42b13facbd759
Meher Kolli <mekolli@exelixis.com>
2026-02-24 14:22:50 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_api_url          = var.pm_api_url
-  pm_api_token_id     = var.pm_api_token_id
-  pm_api_token_secret = var.pm_api_token_secret
+  pm_api_url          = var.PM_API_URL
+  pm_api_token_id     = var.PM_API_TOKEN_ID
+  pm_api_token_secret = var.PM_API_TOKEN_SECRET
   pm_tls_insecure     = var.pm_tls_insecure
 }
 
