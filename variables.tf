aee88e51c8ecf61e3ea748c967c50ada3ef6a03e
Meher Kolli <mekolli@exelixis.com>
2026-02-24 13:53:49 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,16 +2,13 @@
 variable "PM_API_URL" {
   type = string
 }
-
 variable "PM_API_TOKEN_ID" {
   type = string
 }
-
 variable "PM_API_TOKEN_SECRET" {
   type = string
   sensitive = true
 }
-
 variable "pm_tls_insecure" {
   type    = bool
   default = false
